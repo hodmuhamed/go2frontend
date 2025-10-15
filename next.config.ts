073c1e1f5fdbf1cd262e9b0426d548309c705c2b
hodmuhamed <60523308+hodmuhamed@users.noreply.github.com>
2025-10-16 00:44:29 +0200

    Add dynamic category sections to homepage

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,18 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "go2njemacka.de",
+      },
+      {
+        protocol: "https",
+        hostname: "www.go2njemacka.de",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
